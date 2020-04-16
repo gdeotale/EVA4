e86b824b9d6df619bbb3d05da643685f4304881b
Gunjan P Deotale <gdeotale@gmail.com>
2020-04-16 12:17:02 +0530

    Assignment 12

--- Week12/PartA/Download_Data/Download.py
+++ Week12/PartA/Download_Data/Download.py
@@ -3,6 +3,8 @@ import zipfile
 import requests
 import shutil
 import progressbar
+import math
+from tqdm.notebook import tqdm
 from io import StringIO,BytesIO
 def download_image(url):
   if(os.path.isdir(os.getcwd()+"/tiny-imagenet-200.zip")):
@@ -15,14 +17,9 @@ def download_image(url):
   zip_ref.close()
 
 def segregate():
-  bar = progressbar.ProgressBar(maxval=200, \
-    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-  bar.start()
   count1 = 0
-  for root, dirs, files in os.walk(os.getcwd()+'/tiny-imagenet-200/train/'):  # replace the . with your starting directory
+  for root, dirs, files in tqdm(os.walk(os.getcwd()+'/tiny-imagenet-200/train/')):  # replace the . with your starting directory
      count = 0
-     count1 = count1 + 1
-     bar.update(count1/2)
      for file in files:  
         if not os.path.exists(os.getcwd()+'/MergeData/Train/'+file.split('_')[0]+'/'):
              os.makedirs(os.getcwd()+'/MergeData/Train/'+file.split('_')[0]+'/')
@@ -34,9 +31,10 @@ def segregate():
                 shutil.copy(root+'/'+file, os.getcwd()+'/MergeData/Train/'+file.split('_')[0]+'/')
             else:
                 shutil.copy(root+'/'+file, os.getcwd()+'/MergeData/Val/'+file.split('_')[0]+'/')
-  bar.finish()
+  
   text_file = open(os.getcwd()+"/tiny-imagenet-200/val/val_annotations.txt", "r")
   Lines = text_file.readlines()
-  for line in Lines:
+  count1 = 0
+  for line in tqdm(Lines):
     shutil.copy(os.getcwd()+'/tiny-imagenet-200/val/images'+'/'+line.split('\t')[0], 'MergeData/Val/'+line.split('\t')[1]+'/')
   text_file.close()
